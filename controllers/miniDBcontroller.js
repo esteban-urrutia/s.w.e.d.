cca532b2e85af87f9471e15bc1c0ac6dcfe41542
swed <estebanUrrutiaMartinez@gmail.com>
2022-10-26 13:30:04 -0300

    s

--- controllers/miniDBcontroller.js
+++ controllers/miniDBcontroller.js
@@ -18,30 +18,62 @@ async function readMiniDB(semaphoreMiniDB) {
   return new Promise(async (resolve, reject) => {
     semaphoreMiniDB.take(async () => {
       let miniDB;
-      try {
-        // if miniDB.json exist
-        if (fs.existsSync('miniDB.json')) {
-          // read and parse miniDB.json
-          fs.readFile('miniDB.json', async (err, data) => {
-            if (err) throw err;
+
+      // if miniDB.json exist
+      if (fs.existsSync('miniDB.json')) {
+        // read and parse miniDB.json
+        fs.readFile('miniDB.json', async (err, data) => {
+          if (err) {
+            semaphoreMiniDB.leave();
+            reject({
+              message: (err.hasOwnProperty('stack') ? err.stack : err),
+              stack: 'miniDBcontroller -> readMiniDB -> readFile',
+            });
+          }
+
+          try {
             miniDB = await JSON.parse(data.toString());
             semaphoreMiniDB.leave();
             resolve(miniDB);
-          });
-        } else {
-          miniDB = miniDBtemplate;
-          fs.writeFile('miniDB.json', JSON.stringify(miniDB, null, 4), (err) => {
-            if (err) throw err;
-            semaphoreMiniDB.leave();
-            resolve(miniDB);
-          });
-        }
-      } catch (error) {
-        semaphoreMiniDB.leave();
-        reject({
-          data: 'miniDB.json',
-          message: (error.hasOwnProperty('stack') ? error.stack : error),
-          stack: 'miniDBcontroller -> readMiniDB',
+          } catch (error) {
+            try {
+              await execute('sudo rm miniDB.json');
+
+              miniDB = miniDBtemplate;
+              fs.writeFile('miniDB.json', JSON.stringify(miniDB, null, 4), (err) => {
+                if (err) {
+                  semaphoreMiniDB.leave();
+                  reject({
+                    message: (err.hasOwnProperty('stack') ? err.stack : err),
+                    stack: 'miniDBcontroller -> readMiniDB -> parsing error -> writeFile',
+                  });
+                }
+
+                resolve(miniDB);
+              });
+            } catch (error) {
+              semaphoreMiniDB.leave();
+              reject({
+                message: (error.hasOwnProperty('stack') ? error.stack : error),
+                stack: 'miniDBcontroller -> readMiniDB -> parsing error -> deleting error',
+              });
+            }
+          }
+        });
+      }
+      // if miniDB.json don't exist
+      else {
+        miniDB = miniDBtemplate;
+        fs.writeFile('miniDB.json', JSON.stringify(miniDB, null, 4), (err) => {
+          semaphoreMiniDB.leave();
+          if (err) {
+            reject({
+              message: (err.hasOwnProperty('stack') ? err.stack : err),
+              stack: 'miniDBcontroller -> readMiniDB -> writeFile',
+            });
+          }
+
+          resolve(miniDB);
         });
       }
     });
